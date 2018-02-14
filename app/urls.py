208c63f3862076df605de6815dcc83d1adda8c4b
Revenage <romanvolobuev93@gmail.com>
2018-02-14 21:53:53 +0200

    add login logout signup

--- app/urls.py
+++ app/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
     url(r'^schedule/', include('schedule.urls'), name='schedule'),
+    # url(r'^login/$', views.log_in, name='login'),
+    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^logout/$', views.log_out, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
     return render(request, 'app/index.html', {'body_class': 'text-center'})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def log_in(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        auth.login(request, user)
+        return redirect('home')
+    else:
+        return redirect('signup')
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
