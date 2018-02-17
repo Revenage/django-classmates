3f78b10999add5ee448681c086470c0e5a879075
Revenage <romanvolobuev93@gmail.com>
2018-02-17 18:41:18 +0200

    fix errors

--- app/static/js/error_page.js
+++ app/static/js/error_page.js
@@ -1,46 +1,28 @@
-function randomNum()
-{
-    "use strict";
-    return Math.floor(Math.random() * 9)+1;
-}
-    var loop1,loop2,loop3,time=30, i=0, number, selector3 = document.querySelector('.thirdDigit'), selector2 = document.querySelector('.secondDigit'),
-        selector1 = document.querySelector('.firstDigit');
-    loop3 = setInterval(function()
-    {
-      "use strict";
-        if(i > 40)
-        {
-            clearInterval(loop3);
-            selector3.textContent = 4;
-        }else
-        {
-            selector3.textContent = randomNum();
-            i++;
-        }
+"use strict";
+
+(function() {
+  var number = '404';
+  var num_arr = number.split('');
+  function randomNum() {
+    return Math.floor(Math.random() * 9) + 1;
+  }
+  var time = 30,
+    loppArr = []
+  var selector = ['third', '.second', 'first']
+	var i = 0;
+  
+  function run(s,j) {
+  	 var currentselector = document.querySelector('.'+ s + 'Digit');
+    loppArr[j] = setInterval(function() {
+    	var num = j;
+      if (i > num * 40 + 40) {
+        clearInterval(loppArr[num]);
+        currentselector.textContent = num_arr[num];
+      } else {
+        currentselector.textContent = randomNum();
+        i++;
+      }
     }, time);
-    loop2 = setInterval(function()
-    {
-      "use strict";
-        if(i > 80)
-        {
-            clearInterval(loop2);
-            selector2.textContent = 0;
-        }else
-        {
-            selector2.textContent = randomNum();
-            i++;
-        }
-    }, time);
-    loop1 = setInterval(function()
-    {
-      "use strict";
-        if(i > 100)
-        {
-            clearInterval(loop1);
-            selector1.textContent = 4;
-        }else
-        {
-            selector1.textContent = randomNum();
-            i++;
-        }
-    }, time);
+  }
+  selector.map(run)
+})()
